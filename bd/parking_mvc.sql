180b65215e068d6cfc7257c349b233c94562218a
Josesanchez152100 <70731465+Josesanchez152100@users.noreply.github.com>
2021-11-26 18:56:24 -0500

    se actualiza la BD

--- bd/parking_mvc.sql
+++ bd/parking_mvc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-11-2021 a las 01:42:45
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.3
+-- Tiempo de generación: 27-11-2021 a las 00:53:00
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cliente` (
-  `Identificacion` int(11) NOT NULL,
+  `Identificacion` int(10) NOT NULL,
   `NombresCompletos` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
   `NumeroTelefono` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
   `CorreoElectronico` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
@@ -40,16 +40,16 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`Identificacion`, `NombresCompletos`, `NumeroTelefono`, `CorreoElectronico`, `Direccion`) VALUES
-(1, 'Pedro Picapiedra', '1234567', 'pedro@piedradura.com', 'Bedrock'),
-(235246, 'sdgshgh', '344256', 'sdGsgrs', 'srtsrt4542'),
-(532234, 'Alejandro', '4589566523', 'dgs@dgfsg.com', 'cll 57 55'),
-(1234995, 'Jeferson', '224466665', 'dgs@sfgsg.com', 'crr 45 99'),
-(2345566, 'sdgsfg', '4254636', 'fshfshfsd', 'sw435tgghdh'),
-(62787394, 'Juan', '4583225', 'daflkd@dlk.com', 'cll 23'),
-(98665424, 'Carlos Mario', '6042345412', 'inventado@gmail.com', 'Calle 123 # 81 12'),
-(352465346, 'yepes', '245426242', 'adgsdg@dgg.com', 'cr 78 2554'),
-(435246423, 'Paula', '546153', 'asrfaefe@fdg.com', 'crr 78 52'),
-(642345453, 'Jorge', '25634363475', 'zdf@sfgfgg.com', 'crr 243 842');
+(8975642, 'Julio Botero', '320812333', 'julio158@correo.com', 'Cir 4 22 16'),
+(9238475, 'Juan Perez', '3456789', 'juan@gmail.com', 'cr 8 - 9 - 15'),
+(9856423, 'Juan Gallego', '302987120', 'juan.gallego@gmail.com', 'Cl 38 88 57'),
+(42984722, 'Juan Perez', '9875240', 'juanp23@hotmail.com', 'Av 31 - 98 - 10'),
+(78956442, 'Daniel Diaz', '3521879', 'danield12@hotmail.com', 'cr 9 15 37'),
+(1054789663, 'Vanessa Montoya', '45687921', 'vane811@gmail.com', 'cr57 56 98'),
+(1125487987, 'Sebastian Areiza', '305978456', 'sebasa123@gmail.com', 'cr 50 88 104'),
+(1152184023, 'Ana Rendon', '3529184', 'ana.rendon@correo.com', 'Cr 80 #70 19'),
+(1152439020, 'Jose Sanchez', '3045405340', 'fore029@gmail.com', 'Calle 9A sur #79 A 125'),
+(1189756234, 'JULIAN GAVIRIA', '317895462', 'julian895@cesde.com', 'av 32 56 24');
 
 -- --------------------------------------------------------
 
@@ -58,10 +58,10 @@ INSERT INTO `cliente` (`Identificacion`, `NombresCompletos`, `NumeroTelefono`, `
 --
 
 CREATE TABLE `parqueadero` (
-  `Id` int(11) NOT NULL,
-  `CuposTotales` int(11) NOT NULL,
-  `CuposDisponibles` int(11) NOT NULL,
-  `CuposReservados` int(11) NOT NULL
+  `Id` int(10) NOT NULL,
+  `CuposTotales` int(10) NOT NULL,
+  `CuposDisponibles` int(10) NOT NULL,
+  `CuposReservados` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `parqueadero` (
 --
 
 INSERT INTO `parqueadero` (`Id`, `CuposTotales`, `CuposDisponibles`, `CuposReservados`) VALUES
-(1, 20, 19, 1);
+(1, 30, 21, 9);
 
 -- --------------------------------------------------------
 
@@ -94,13 +94,16 @@ CREATE TABLE `vehiculo` (
 --
 
 INSERT INTO `vehiculo` (`Placa`, `IdCliente`, `IdParqueadero`, `Marca`, `Color`, `Modelo`, `Estado`, `HoraIngreso`, `HoraSalida`) VALUES
-('111aaa', 235246, 1, 'Mazda', 'Blanco', 'Serie 3 2020', 0, '2021-11-21 14:00:00', '2021-11-21 22:15:50'),
-('111bbb', 2345566, 1, 'Chevrolet', 'Blanco', 'Corsa 1999', 0, '2021-11-21 14:22:10', '2021-11-21 22:17:01'),
-('123pru', 98665424, 1, 'Volvo', 'Gris plateado', 'Recharge 2015', 0, '2021-11-21 10:15:41', '2021-11-21 22:26:09'),
-('123ROK', 1, 1, 'Rocamovil', 'Cafe', 'Rock 10000 AC', 1, '2021-11-20 12:15:00', '2021-11-20 23:31:00'),
-('555fff', 1234995, 1, 'Toyota', 'Gris', 'Corolla 2018', 1, '2021-11-21 13:44:34', '2021-11-21 16:03:06'),
-('644tre', 642345453, 1, 'Toyota', 'Plateado', 'Fortuner 2018', 1, '2021-11-21 13:33:05', '2021-11-21 21:35:19'),
-('987qwe', 352465346, 1, 'Volvo', 'Azul', 'XC40 2017', 1, '2021-11-21 13:02:45', NULL);
+('asd342', 9238475, 1, 'mazda', 'cyan', 'CX-30', 0, '2021-11-26 13:55:28', '2021-11-26 13:58:17'),
+('JAR111', 78956442, 1, 'OPEL', 'VINOTINTO', 'ASTRA', 1, '2021-11-26 12:45:55', NULL),
+('KAP112', 1054789663, 1, 'VOLVO', 'GRIS', 'S60', 1, '2021-11-26 12:47:58', NULL),
+('KIP876', 1152184023, 1, 'Chevrolet', 'Rojo', 'Spark GT', 1, '2021-11-26 03:01:21', NULL),
+('NKV378', 1152439020, 1, 'MAZDA', 'AZUL', 'CX-30', 1, '2021-11-26 02:53:50', NULL),
+('NQW555', 8975642, 1, 'HYUNDAI', 'PLATA', 'TUCSON', 1, '2021-11-26 12:54:12', NULL),
+('PER917', 42984722, 1, 'KIA', 'Negro', 'Picanto', 1, '2021-11-26 03:08:11', NULL),
+('PWR487', 1189756234, 1, 'NISSAN', 'BLANCO', 'VERSA', 1, '2021-11-26 12:44:01', NULL),
+('QAP187', 9856423, 1, 'SUSUKI', 'VERDE', 'JIMNY LITE', 1, '2021-11-26 12:37:42', NULL),
+('TUY213', 1125487987, 1, 'RENAULT', 'AMARILLO', 'SANDERO', 1, '2021-11-26 12:41:56', NULL);
 
 --
 -- Índices para tablas volcadas
